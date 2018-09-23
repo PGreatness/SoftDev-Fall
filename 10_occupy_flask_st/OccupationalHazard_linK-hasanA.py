2f5fe837dd57b011f92d2fee27326e3f78662239
Ahnaf Hasan <ahasan1@stuy.edu>
2018-09-22 21:08:19 -0400

    merged with partner(Karen Li)

--- 10_occupy_flask_st/OccupationalHazard_linK-hasanA.py
+++ 10_occupy_flask_st/OccupationalHazard_linK-hasanA.py
@@ -1,7 +1,10 @@
+# OccupationalHazard: Karen Li, Ahnaf Hasan
+# SoftDev1 pd07
+# K #10: Jinja Tuning 
+# 2018 - 09 - 24
+
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
 import csv, random
 
 
@@ -30,7 +33,7 @@ def fillList():
 
 	    
 
-       	    #fills occupationList with occupations with frequency dependent on percentagE
+       	    #fills occupationList with occupations with frequency dependent on percentage
 
             occupationDic[row['Job Class']] = row['Percentage']
 
--- KarenLi/SoftDev/README.md
+++
@@ -1,1 +0,0 @@
-# LKaren_SoftDev
