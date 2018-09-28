40d01b612afc812cf775161b5148ed564c87ad11
Ahnaf Hasan <ahasan1@stuy.edu>
2018-09-27 23:23:00 -0400

    add comments

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -1,3 +1,10 @@
+'''
+Ahnaf Hasan
+SoftDev pd07
+K13 Echo Echo Echo
+2018-09-27
+'''
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -5,18 +12,18 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-    return render_template("firstPage.html",
-                            daWay = 'GET')
+    return render_template("firstPage.html", #render the template
+                            daWay = 'GET') #choose between GET and POST
 
-@app.route("/auth", methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET', 'POST']) #courtesy of Bill Ni's thread
 def authenticate():
-    if request.method == 'GET':
+    if request.method == 'GET': #daWay is GET
         return render_template("secondPage.html",
-                                user = request.args['username'],
-                                method = request.method)
-    else:
-        return render_template("secondPage.html",
-                                user = "Anon",
+                                user = request.args['username'], #POST doesn't store the data in the URL
+                                method = request.method) #method used, either GET or POST
+    else:  #POST request used, sensitive data, shhh!
+        return render_template("secondPage.html", #the page where clients are taken to
+                                user = "Anon", #user value isn't stored, so has to be done manually
                                 method = request.method)
 
 
