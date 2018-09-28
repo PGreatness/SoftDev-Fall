5601f656653d01ac930d87e0cb485f181a7c88ef
Ahnaf Hasan <ahasan1@stuy.edu>
2018-09-27 23:09:48 -0400

    added implementation of GET and POST

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -5,13 +5,19 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-    return render_template("firstPage.html")
+    return render_template("firstPage.html",
+                            daWay = 'GET')
 
-@app.route("/auth")
+@app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
-    return render_template("secondPage.html",
-                               user = request.args['username'],
-                               method = request.method)
+    if request.method == 'GET':
+        return render_template("secondPage.html",
+                                user = request.args['username'],
+                                method = request.method)
+    else:
+        return render_template("secondPage.html",
+                                user = "Anon",
+                                method = request.method)
 
 
 if __name__ == "__main__":
