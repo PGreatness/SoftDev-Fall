ba4433055dbb4398488be645d5ebaff787f08635
Ahnaf Hasan <ahasan1@stuy.edu>
2018-10-02 00:19:35 -0400

    merged with partner(YeJ)

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,68 +1,36 @@
-import os
+#Therapy Session -- Team Bob: Jerry Ye and Ahnaf Hasan
+#SoftDev1 pd7
+#K14 -- Do I Know You?
+#2018-10-01
 
-from flask import Flask, render_template, request, session, url_for, redirect
+from flask import Flask, render_template, session, redirect, url_for, request #imports class Flask
+app = Flask(__name__)#Creates an instance of Flask
 
-app = Flask(__name__)
-
-app.secret_key = os.urandom(32) #shhhhhh!!!!
-
-
-@app.route('/')
+app.secret_key = '\x86\xeb>/%1\xa6S\xd0\xde\xd0\nf\r\x95\x02%\xb3\x94m\x82\x03\xd4=\xb4\xf0\x87\xa36\x80\x07\xfc'
+@app.route('/')#Defines index
 def index():
-    if 'username' in session: #check if user is already logged in
-        print("true")
-        return render_template("successPage.html") #they are logged in already, move them to the end page
-    print("flase")
-    return render_template('welcomePage.html') #new user, who dis?
-
-@app.route('/login')
-def log():
-    return render_template("loginPage.html") #send them there to log in
-
-@app.route('/register')
-def register():
-    return render_template("registerPage.html") #registry, WIP
-
-@app.route('/redir')
-def addToCvs():
-    file = open("data/userPass.csv", "a")
-    usr = request.form.get('newUser')
-    passwrd = request.form.get('newPass')
-    file.write(usr + "," + passwrd)
-    return redirect(url_for("/login"))
-
-@app.route('/auth', methods=['POST']) #method must be POST because sensitve data
+    if 'username' in session:
+        return render_template("welcome.html", user = session['username'])
+    else:
+        return render_template("login.html")
+@app.route('/auth', methods=['GET','POST'])
 def authenticate():
-    file = open("data/userPass.csv", "r") #file reading from 03_occupation, see on GitHub
-    userDict = {}
-    for line in file:
-        i = line.find(",")
-        if i == -1: #csv file is missing, user list gone <:(
-            return render_template("loginError.html",
-                                    errMessage = "Internal Server Error",
-                                    errCode = 500)
-        usr = line[:i]
-        passwrd = line[i + 1:]
-        userDict[usr] = passwrd
-    if userDict.get(request.form.get('username'), "None") == "None": #user is not in this csv file
-        print("First triggered.")
-        return render_template("loginError.html",
-                                errMessage = "Unrecognized username, please try again or register below",
-                                errCode = 205) #random error code makes it look legit
-    if userDict.get(request.form.get('username'), "None") != request.form.get('password'): #username is there but password is wrong
-        return render_template("loginError.html",
-                                errMessage = "Unrecognized password, please try again",
-                                errCode = 206) #random error code makes it look legit
-    if userDict.get(request.form.get('username'), "None") == request.form.get('password'): #username and password match, BINGO!!!
-        session['username'] = request.form.get('username') #add user to session, keeps logged in on other tabs
-        print("Third triggered.")
-        return render_template("successPage.html") #Success
+    if request.form['username'] == 'Bob' and request.form['pass'] == 'isAwesome':
+        session['username'] = 'Bob'
+    elif request.form['username'] == 'Bob':
+        return "Incorrect password!"
+    elif request.form['pass'] == 'isAwesome':
+        return "Incorrect Username!"
+    else:
+        return "Incorrect password and username!"
+    return redirect(url_for("index"))
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for("index"))
 
-@app.route('/end')
-def endSession():
-    session.pop('username', None) #remove user and say goodbye
-    return redirect(url_for('index')) #send to home page
 
-if __name__ == "__main__":
-    app.debug = True
+
+if (__name__) == "__main__":#if this file is run directly then the Flask app will run
+    app.debug = True#allows changes to directly affect local host without rerunning app
     app.run()
